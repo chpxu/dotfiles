3d46fd8944f90d732175c6deb396462073cf2a8f
chpxu <hi.progracientist@outlook.com>
2022-12-14 21:30:09 +0000

    Hyprland highrr

--- hm/legion/common/modules/hyprland/hyprland.nix
+++ hm/legion/common/modules/hyprland/hyprland.nix
@@ -4,7 +4,7 @@
       exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
       exec-once=systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
       #FIXME
-      monitor=eDP-1,2560x1600@144,0x0,1,bitdepth,10
+      monitor=eDP-1,highrr,0x0,1,bitdepth,10
     ''
     + (import ../../../../common/modules/hyprland/commonConfig.nix).commonConfig;
 }
