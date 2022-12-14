049ea4f42a66a66d2b43e8db3ee0768603e83e6f
chpxu <hi.progracientist@outlook.com>
2022-12-14 21:22:53 +0000

    Fix long app load time and try hrr

--- hm/legion/common/modules/hyprland/hyprland.nix
+++ hm/legion/common/modules/hyprland/hyprland.nix
@@ -1,8 +1,10 @@
 {
   extraConfig =
     ''
+      exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+      exec-once=systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
       #FIXME
-      monitor=eDP-1,preferred,0x0,1,bitdepth,10
+      monitor=eDP-1,2560x1600@144,0x0,1,bitdepth,10
     ''
     + (import ../../../../common/modules/hyprland/commonConfig.nix).commonConfig;
 }
