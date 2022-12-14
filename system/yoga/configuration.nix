4456fcd4133d72f525c1fc51e1cde2d16fb7d8ae
chpxu <hi.progracientist@outlook.com>
2022-12-14 19:20:13 +0000

    Remove copySystemConfiguration

--- system/yoga/configuration.nix
+++ system/yoga/configuration.nix
@@ -350,7 +350,7 @@
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+  # system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
