eed783e9c0d9c4be55bcf54b779899c006ff52c5
chpxu <hi.progracientist@outlook.com>
2022-12-15 11:36:36 +0000

    hyprpaper, home decomposition

--- hm/legion/chunix/home.nix
+++ hm/legion/chunix/home.nix
@@ -15,65 +15,15 @@
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     WLR_NO_HARDWARE_CURSORS = 1;
   };
-  # GTK themes
-  gtk = rec {
-    enable = true;
-    theme = {
-      name = "Nordic";
-      package = pkgs.nordic;
-    };
-    iconTheme = {
-      name = "Nordzy";
-      package = pkgs.nordzy-icon-theme;
-    };
-    cursorTheme = {
-      name = "Nordzy-cursors";
-      package = pkgs.nordzy-cursor-theme;
-      size = 32;
-    };
-    gtk2 = {
-      configLocation = "${config.home.homeDirectory}/.gtkrc-2.0";
-    };
-  };
-  dconf = {
-    settings = {
-      "org/gnome/desktop/interface" = {
-        gtk-theme = "${config.gtk.theme.name}";
-        # cursor-theme = "${config.gtk.cursorTheme.name}";
-      };
-      "org/gnome/desktop/wm/preferences" = {
-        theme = "${config.gtk.theme.name}";
-      };
-    };
-  };
+
   nixpkgs = {
-    # You can add overlays here
     overlays = with outputs.overlays; [
-      # If you want to use overlays your own flake exports (from overlays dir):
       hyprpaper
       xournalpp
       waybar
       discord-canary
       wvkbd
-      # outputs.overlays.additions
-      # outputs.overlays
-      # Or overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
-    };
   };
-  programs.home-manager.enable = true;
+  imports = [../../common/commonHome.nix];
 }
---
+++ hm/legion/common/commonHome.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  gtk = rec {
+    enable = true;
+    theme = {
+      name = "Nordic";
+      package = pkgs.nordic;
+    };
+    iconTheme = {
+      name = "Nordzy";
+      package = pkgs.nordzy-icon-theme;
+    };
+    cursorTheme = {
+      name = "Nordzy-cursors";
+      package = pkgs.nordzy-cursor-theme;
+      size = 32;
+    };
+    gtk2 = {
+      configLocation = "${config.home.homeDirectory}/.gtkrc-2.0";
+    };
+  };
+  dconf = {
+    settings = {
+      "org/gnome/desktop/interface" = {
+        gtk-theme = "${config.gtk.theme.name}";
+        cursor-theme = "${config.gtk.cursorTheme.name}";
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        theme = "${config.gtk.theme.name}";
+      };
+    };
+  };
+  nixpkgs.config = {
+    # Disable if you don't want unfree packages
+    allowUnfree = true;
+    # Workaround for https://github.com/nix-community/home-manager/issues/2942
+    allowUnfreePredicate = _: true;
+  };
+  programs.home-manager.enable = true;
+}
