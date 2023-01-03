b538ce83c58c4f23c0c0c8ec21a092a306c6c66a
chpxu <dev.chpxu@outlook.com>
2023-01-03 19:30:11 +0000

    Remove failed hack

--- hm/common/modules/default.nix
+++ hm/common/modules/default.nix
@@ -5,30 +5,30 @@
   colour-palette,
   ...
 }: {
-  # imports = [
-  #   # ./wayfire
-  #   ./Discord
-  #   ./Code
-  #   ./gh_git
-  #   ./waybar
-  #   ./zathura
-  #   ./mpv
-  #   (import ./kitty {inherit pkgs colour-palette;})
-  #   ./mako
-  #   ./sway
-  #   # ./kanshi
-  #   ./xdg/xdg.nix
-  #   #//texlive.nix
-  #   ./python
-  #   ./firefox
-  #   # ./neovim
-  #   ./wofi
-  #   # ./zsh
-  #   ./hyprland/xdg.nix
-  #   # ./starship
-  #   ./jq
-  #   # ./modules/osu-stable
-  #   # ./modules/wireplumber
-  # ];
-  imports = builtins.map (dir: (import (builtins.toPath ./. + "/${dir}") {inherit pkgs colour-palette;})) (builtins.attrNames (builtins.readDir ./.));
+  imports = [
+    # ./wayfire
+    ./Discord
+    ./Code
+    ./gh_git
+    ./waybar
+    ./zathura
+    ./mpv
+    (import ./kitty {inherit pkgs colour-palette;})
+    ./mako
+    ./sway
+    # ./kanshi
+    ./xdg/xdg.nix
+    #//texlive.nix
+    ./python
+    ./firefox
+    # ./neovim
+    ./wofi
+    # ./zsh
+    ./hyprland/xdg.nix
+    # ./starship
+    ./jq
+    # ./modules/osu-stable
+    # ./modules/wireplumber
+  ];
+  # imports = builtins.map (dir: (import (./. + "/${dir}") {inherit pkgs colour-palette;})) (builtins.attrNames (builtins.readDir (builtins.toPath ./. + "/")));
 }
