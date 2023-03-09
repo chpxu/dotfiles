3c08e6455d601b018ea294e63e3953d15dfd0cb6
chpxu <hi.progracientist@outlook.com>
2023-03-09 23:37:41 +0000

    Move xdg-desktop-portal to new option

--- system/common/modules/xdg.nix
+++ system/common/modules/xdg.nix
@@ -3,9 +3,7 @@
     icons.enable = true;
     portal = {
       enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-      ];
+      wlr.enable = true;
     };
   };
 }
