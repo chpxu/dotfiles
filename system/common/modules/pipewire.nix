d05b7d45e95afa99812d0bd12576c0edbe327070
chpxu <hi.progracientist@outlook.com>
2023-04-02 16:46:34 +0100

    Update deps and pipewire

--- system/common/modules/pipewire.nix
+++ system/common/modules/pipewire.nix
@@ -1,4 +1,18 @@
 {
+  environment.etc = {
+    "pipewire.conf" = {
+      enable = true;
+      source = ./pipewire/pipewire.conf;
+      mode = "0644";
+      target = "pipewire.conf.d/pipewire.conf";
+    };
+    "pipewire-pulse.conf" = {
+      enable = true;
+      source = ./pipewire/pipewire-pulse.conf;
+      mode = "0644";
+      target = "pipewire.conf.d/pipewire-pulse.conf";
+    };
+  };
   services.pipewire = {
     enable = true;
     audio.enable = true;
@@ -6,93 +20,5 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
-    # lowLatency.enable = true;
-
-    # # defaults (no need to be set unless modified)
-    # lowLatency = {
-    #   quantum = 48;
-    #   rate = 48000;
-    # };
-    config.pipewire = {
-      "context.properties" = {
-        "link.max-buffers" = 16;
-        "log.level" = 2;
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 64;
-        "default.clock.min-quantum" = 2;
-        "default.clock.max-quantum" = 256;
-        "core.daemon" = true;
-        "core-name" = "pipewire-0";
-      };
-      "context.modules" = [
-        {
-          name = "libpipewire-module-rtkit";
-          args = {
-            "nice.level" = -15;
-            "rt.prio" = 88;
-            "rt.time.soft" = -1;
-            "rt.time.hard" = -1;
-          };
-          flags = ["ifexists" "nofail"];
-        }
-        {name = "libpipewire-module-protocol-native";}
-        {name = "libpipewire-module-profiler";}
-        {name = "libpipewire-module-metadata";}
-        {name = "libpipewire-module-spa-device-factory";}
-        {name = "libpipewire-module-spa-node-factory";}
-        {name = "libpipewire-module-client-node";}
-        {name = "libpipewire-module-client-device";}
-        {
-          name = "libpipewire-module-portal";
-          flags = ["ifexists" "nofail"];
-        }
-        {
-          name = "libpipewire-module-access";
-          args = {};
-        }
-        {name = "libpipewire-module-adapter";}
-        {name = "libpipewire-module-link-factory";}
-        {name = "libpipewire-module-session-manager";}
-      ];
-    };
-    config.pipewire-pulse = {
-      "context.properties" = {
-        "log.level" = 2;
-      };
-      "context.modules" = [
-        {
-          name = "libpipewire-module-rtkit";
-          args = {
-            "nice.level" = -15;
-            "rt.prio" = 88;
-            "rt.time.soft" = -1;
-            "rt.time.hard" = -1;
-          };
-          flags = ["ifexists" "nofail"];
-        }
-        {name = "libpipewire-module-protocol-native";}
-        {name = "libpipewire-module-client-node";}
-        {name = "libpipewire-module-adapter";}
-        {name = "libpipewire-module-metadata";}
-        {
-          name = "libpipewire-module-protocol-pulse";
-          args = {
-            "pulse.min.req" = "48/48000";
-            "pulse.default.req" = "48/48000";
-            "pulse.max.req" = "48/48000";
-            "pulse.min.quantum" = "48/48000";
-            "pulse.max.quantum" = "48/48000";
-            "server.address" = ["unix:native"];
-          };
-        }
-      ];
-      "stream.properties" = {
-        "node.latency" = "48/48000";
-        "resample.quality" = 4;
-      };
-      "pulse.properties" = {
-        "server.address" = ["unix:native"];
-      };
-    };
   };
 }
--- system/common/modules/zsh.nix
+++ system/common/modules/zsh.nix
@@ -3,15 +3,15 @@
   programs.zsh = {
     enable = true;
     #loginExtra = "betterdiscordctl --d-modules ~/config/discordcanary/00136/modules/ install";
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    enableCompletion = true;
-    ohMyZsh = {
-      enable = true;
-    };
-  };
-  programs.starship = {
-    enable = true;
-    settings = builtins.fromTOML (builtins.readFile ./starship/startship.toml);
+    #   autosuggestions.enable = true;
+    #   syntaxHighlighting.enable = true;
+    #   enableCompletion = true;
+    #   ohMyZsh = {
+    #     enable = true;
+    #   };
+    # };
+    # programs.starship = {
+    #   enable = true;
+    #   settings = builtins.fromTOML (builtins.readFile ./starship/startship.toml);
   };
 }
