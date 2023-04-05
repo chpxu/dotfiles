b878875b402316cb054d30ef92fca802b462e6cd
chpxu <dev.chpxu@outlook.com>
2023-04-05 11:37:23 +0100

    update deps

--- hm/legion/common/modules/hyprland/hyprland.nix
+++ hm/legion/common/modules/hyprland/hyprland.nix
@@ -5,16 +5,9 @@
       #exec-once=systemctl --user import-environment WAYLAND_DISPLAY
       #XDG_CURRENT_DESKTOP
       #FIXME
-      monitor=eDP-1,2560x1600@165.003998,0x0,1,bitdepth,10
-      monitor=eDP-2,2560x1600@165.003998,0x0,1,bitdepth,10
-      monitor=HDMI-A-1,1920x1080@144.001007,2560x0,1
-
-      decoration {
-        blur = false
-      }
-      misc {
-        no_vfr = false
-      }
+      monitor=eDP-1,2560x1600@165.003998,0x0,1
+      #monitor=eDP-2,2560x1600@165.003998,0x0,1,bitdepth,10
+      #monitor=HDMI-A-1,1920x1080@144.001007,2560x0,1
     ''
     + (import ../../../../common/modules/hyprland/commonConfig.nix).commonConfig;
 }
