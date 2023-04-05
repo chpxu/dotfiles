af47f3fad0405ad47b4ac1d13c5d52f392967f77
chpxu <dev.chpxu@outlook.com>
2023-04-05 12:05:05 +0100

    Clean up

--- hm/common/packages/default.nix
+++ hm/common/packages/default.nix
@@ -1,12 +1,7 @@
-{
-  pkgs,
-  needsNvidia,
-  ...
-}: {
+{...}: {
   imports = [
-    # ./python/python.nix
     ./daily
     ./dev
-    (import ./environment.nix {inherit pkgs needsNvidia;})
+    ./environment.nix
   ];
 }
--- hm/common/packages/python/python.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: let
-  pythonEnv = import ./definePythonEnv.nix;
-  customPythonEnv = pkgs.python310.withPackages pythonEnv.my-python-pkgs;
-in {
-  home.packages = with pkgs; [
-    pipenv
-    customPythonEnv
-  ];
-}
