9160ee5d53e1e80aa0893f002cea012ca65ad0f8
chpxu <dev.chpxu@outlook.com>
2023-06-10 10:22:23 +0100

    Make nvidia module optional

--- system/common/modules/boot.nix
+++ system/common/modules/boot.nix
@@ -1,11 +1,12 @@
 {
   config,
   pkgs,
+  lib,
+  needsNvidia,
   ...
 }: {
   boot = {
     loader = {
-      # Use the systemd-boot EFI boot loader.
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
       grub.devices = ["/dev/nvme0n1"];
@@ -13,16 +14,11 @@
       grub.efiSupport = true;
       grub.useOSProber = true;
     };
-    # initrd = {
-    #   kernelModules = ["nvidia"];
-    # };
     kernelPackages = pkgs.linuxPackages_xanmod_latest;
-    kernelModules = ["uinput" "acpi_call" "nvidia"];
-    extraModulePackages = with config.boot.kernelPackages; [acpi_call nvidiaPackages.beta];
-    # extraModulePackages = with pkgs.linuxKernel.packages.linux_xanmod_latest [acpi_call nvidia_x11];
+    kernelModules = ["uinput" "acpi_call"] ++ lib.optional needsNvidia ["nvidia"];
+    extraModulePackages = with config.boot.kernelPackages; [acpi_call] ++ lib.optional needsNvidia [config.boot.kernelPackagesnvidiaPackages.beta];
     blacklistedKernelModules = ["nouveau"];
     kernelParams = ["ibt=off" "module_blacklist=nouveau" "i915.enable_psr=0" "i915.enable_fbc=1" "i915.fastboot=0" "i915.enable_dc=0" "i915.enable_guc=3"];
-    #kernelParams = ["ibt=off" "module_blacklist=nouveau"];
     initrd.supportedFilesystems = ["btrfs"];
     supportedFilesystems = ["ntfs" "btrfs"];
   };
