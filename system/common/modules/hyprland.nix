c1094fa473c8a15feec69c27c5507b4cb6ec7992
chpxu <dev.chpxu@outlook.com>
2024-07-11 13:48:34 +0100

    use hyprland from nixpkgs and fix dbus

--- system/common/modules/hyprland.nix
+++ system/common/modules/hyprland.nix
@@ -5,7 +5,7 @@
 }: {
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    package = pkgs.hyprland;
     portalPackage = pkgs.xdg-desktop-portal-hyprland;
   };
 }
