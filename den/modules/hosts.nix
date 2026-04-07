0d4cb671de94c90d480c95ffd9e7234f1a38cb3f
chpxu <dev.chpxu@outlook.com>
2026-04-07 14:30:17 +0100

    make vm autogenerate per host

--- den/modules/hosts.nix
+++
@@ -1,7 +0,0 @@
-{
-  den.hosts.x86_64-linux.jingliu = {
-    users.chunix = {};
-    gpu = "nvidia";
-    offload = true;
-  };
-}
--- den/modules/vm.nix
+++ den/modules/vm.nix
@@ -1,22 +1,40 @@
-# enables `nix run .#vm`. it is very useful to have a VM
-# you can edit your config and launch the VM to test stuff
-# instead of having to reboot each time.
+# # enables `nix run .#vm`. it is very useful to have a VM
+# # you can edit your config and launch the VM to test stuff
+# # instead of having to reboot each time.
 {
   inputs,
   den,
+  lib,
   ...
-}: {
-  # USER TODO: remove this tty-autologin used for the VM
-  den.aspects.jingliu.includes = [(den.provides.tty-autologin "chunix")];
+}:
 
-  perSystem = {pkgs, ...}: {
-    packages.vm = pkgs.writeShellApplication {
-      name = "vm";
-      text = let
-        host = inputs.self.nixosConfigurations.jingliu.config;
-      in ''
-        ${host.system.build.vm}/bin/run-${host.networking.hostName}-vm "$@"
-      '';
+{
+  # USER TODO: remove this tty-autologin used for the VM
+  den.aspects.jingliu.includes = [ (den.provides.tty-autologin "chunix") ];
+  den.aspects.saitama.includes = [ (den.provides.tty-autologin "chunix") ];
+
+  perSystem =
+    { pkgs, ... }:
+    let
+      mkVM =
+        hostname:
+        pkgs.writeShellApplication {
+          name = "vm-${hostname}";
+          text =
+            let
+              host = inputs.self.nixosConfigurations.${hostname}.config;
+            in
+            ''
+              ${host.system.build.vm}/bin/run-${host.networking.hostName}-vm "$@"
+            '';
+        };
+    in
+    {
+      packages = lib.listToAttrs (
+        map (hostname: {
+          name = "vm-${hostname}";
+          value = mkVM hostname;
+        }) (builtins.attrNames inputs.self.nixosConfigurations)
+      );
     };
-  };
 }
